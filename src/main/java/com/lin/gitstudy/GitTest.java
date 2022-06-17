be54c55055729befb85eedebe8fee794eb2f8cc5
LinMouren <lgl13727753294@163.com>
2022-06-17 21:14:05 +0800

    IDeaGiteeMaster

--- src/main/java/com/lin/gitstudy/GitTest.java
+++ src/main/java/com/lin/gitstudy/GitTest.java
@@ -2,6 +2,6 @@ package com.lin.gitstudy;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello Gitee Master!");
+        System.out.println("Hello Gitee Master!!");
     }
 }
