73c310d89e10c58305eec309b09931a4323b2982
LinMouren <lgl13727753294@163.com>
2022-06-17 15:21:12 +0800

    IDeaSecondCommit

--- src/main/java/com/lin/gitstudy/GitTest.java
+++ src/main/java/com/lin/gitstudy/GitTest.java
@@ -3,5 +3,6 @@ package com.lin.gitstudy;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git2!");
     }
 }
