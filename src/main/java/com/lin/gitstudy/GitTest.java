0f4f0c5d5a5037765151ba769c8860534d9a0b49
LinMouren <lgl13727753294@163.com>
2022-06-17 20:54:00 +0800

    IDeaMasterLastCommit

--- src/main/java/com/lin/gitstudy/GitTest.java
+++ src/main/java/com/lin/gitstudy/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("master commit");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("masterLast");
     }
 }
