5fb19ce874e5e68ec4ddaa49c27c5a092b565a3a
LinMouren <lgl13727753294@163.com>
2022-06-17 21:11:02 +0800

    IDeaGiteeMaster

--- src/main/java/com/lin/gitstudy/GitTest.java
+++ src/main/java/com/lin/gitstudy/GitTest.java
@@ -2,14 +2,6 @@ package com.lin.gitstudy;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
-        System.out.println("Hello Git2!");
-        System.out.println("Hello Git3!");
-        System.out.println("Hello Git4_hot-fix!");
-        System.out.println("hot-fix commit");
-        System.out.println("master commit");
-        System.out.println("push test");
-        System.out.println("pull test");
-        System.out.println("masterLast");
+        System.out.println("Hello Gitee Master!");
     }
 }
